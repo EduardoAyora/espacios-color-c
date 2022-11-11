9425f7bc8d9ee29795118ed081f99ccec0d09a1f
EduardoAyora <eduardoaayora64@gmail.com>
2022-11-11 10:34:58 -0500

    Finalizar

--- Principal.cpp
+++ Principal.cpp
@@ -6,34 +6,16 @@
 #include <cmath>
 #include <sstream>
 #include <random>
-
-// Librerías de OpenCV
-// Librería que contiene las definiciones de las estructuras básicas
-// para representar imágenes a través de Matrices y sus operaciones
 #include <opencv2/core/core.hpp>
-
-// Librería para preprocesamiento y manipulación de imágenes
 #include <opencv2/imgproc/imgproc.hpp>
-
-// Librería para creación de ventanas y manejo de GUI
 #include <opencv2/highgui/highgui.hpp>
-
-// Librería para manejar los códecs de los diferentes formatos de imágenes
 #include <opencv2/imgcodecs/imgcodecs.hpp>
-
-// Librería para lectura de vídeo
 #include <opencv2/video/video.hpp>
-
-// Librería para escritura de vídeo
 #include <opencv2/videoio/videoio.hpp>
-
 #include <chrono>
 
-// Espacio de nombres de OpenCV
 using namespace cv;
-
-using namespace std; // Funciones propias de C++
-
+using namespace std;
 using namespace std::chrono;
 
 Mat frame;
@@ -41,22 +23,29 @@ Mat gris;
 Mat binaria;
 Mat hsv;
 Mat efecto;
+Mat colorNiveles;
+vector<Mat> canales;
 
 int niveles = 0;
+int nivelesAzul = 0;
+int nivelesVerde = 0;
+int nivelesRojo = 0;
 int pixel;
 int nivel = 0, hefe = 0;
 
-int nNiveles(int pixel)
+int nNiveles(int pixel, int niveles)
 {
-  if (niveles == 0) return 0;
+  if (niveles == 0)
+    return 0;
   double rango_niveles = 256 / niveles;
-  int numero_nivel = (int) pixel / rango_niveles;
+  int numero_nivel = (int)pixel / rango_niveles;
   int rango_inferior = numero_nivel * rango_niveles;
   return rango_inferior;
 }
 
 void eventoTrack(int v, void *data)
 {
+  imwrite("imagenEfecto.jpg", efecto);
 }
 
 int main(int argc, char *argv[])
@@ -70,9 +59,13 @@ int main(int argc, char *argv[])
   namedWindow("Video", WINDOW_AUTOSIZE);
   namedWindow("Efecto", WINDOW_AUTOSIZE);
   namedWindow("Imagen Niveles Gris", WINDOW_AUTOSIZE);
+  namedWindow("Imagen Niveles Colores", WINDOW_AUTOSIZE);
 
   createTrackbar("Nivel gris", "Video", &niveles, 255, eventoTrack, NULL);
-  createTrackbar("Efecto", "Video", &hefe, 4, eventoTrack, NULL);
+  createTrackbar("Nivel azul", "Video", &nivelesAzul, 255, eventoTrack, NULL);
+  createTrackbar("Nivel verde", "Video", &nivelesVerde, 255, eventoTrack, NULL);
+  createTrackbar("Nivel rojo", "Video", &nivelesRojo, 255, eventoTrack, NULL);
+  createTrackbar("Efecto", "Video", &hefe, 3, eventoTrack, NULL);
 
   while (true)
   {
@@ -80,41 +73,41 @@ int main(int argc, char *argv[])
     resize(frame, frame, Size(), 0.5, 0.5);
     cvtColor(frame, gris, COLOR_BGR2GRAY);
 
+    split(frame, canales);
     // Parte 1
     for (int i = 0; i < gris.rows; i++)
     {
       for (int j = 0; j < gris.cols; j++)
       {
         int pixel = gris.at<uchar>(i, j);
-        gris.at<uchar>(i, j) = nNiveles(pixel);
+        gris.at<uchar>(i, j) = nNiveles(pixel, niveles);
+
+        int pixelBlue = canales[0].at<uchar>(i, j);
+        int pixelGreen = canales[1].at<uchar>(i, j);
+        int pixelRed = canales[2].at<uchar>(i, j);
+
+        canales[0].at<uchar>(i, j) = nNiveles(pixelBlue, nivelesAzul);
+        canales[1].at<uchar>(i, j) = nNiveles(pixelGreen, nivelesVerde);
+        canales[2].at<uchar>(i, j) = nNiveles(pixelRed, nivelesRojo);
       }
     }
 
+    merge(canales, colorNiveles);
+
     // Parte 2
     if (hefe == 0)
-    {
       cvtColor(frame, efecto, COLOR_BGR2Lab);
-      imwrite("imagenEfecto.jpg", efecto);
-    }
     else if (hefe == 1)
-    {
       cvtColor(frame, efecto, COLOR_BGR2YCrCb);
-      imwrite("imagenEfecto.jpg", efecto);
-    }
     else if (hefe == 2)
-    {
       cvtColor(frame, efecto, COLOR_BGR2HSV);
-      imwrite("imagenEfecto.jpg", efecto);
-    }
     else if (hefe == 3)
-    {
       cvtColor(frame, efecto, COLOR_BGR2GRAY);
-      imwrite("imagenEfecto.jpg", efecto);
-    }
 
     cv::imshow("Video", frame);
     cv::imshow("Efecto", efecto);
     cv::imshow("Imagen Niveles Gris", gris);
+    cv::imshow("Imagen Niveles Colores", colorNiveles);
 
     if (waitKey(23) == 27)
       break;
